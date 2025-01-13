18354a4a42a24c560d7a0062a0d41724c63e3324
DorzelRamos <dorzel.ramos@gmail.com>
2025-01-13 16:11:43 -0600

    completed mini modal css project

--- main.js
+++ main.js
@@ -4,3 +4,17 @@ const closeBtn = document.querySelector('#close-modal')
 
 openBtn.addEventListener('click', ()=> dialog.showModal());
 closeBtn.addEventListener('click', () => dialog.close());
+
+// close modal when clicking outside
+dialog.addEventListener('click', event => {
+    const rect = dialog.getBoundingClientRect();
+    const isInDialog =
+      event.clientX >= rect.left &&
+      event.clientX <= rect.right &&
+      event.clientY >= rect.top &&
+      event.clientY <= rect.bottom;
+  
+    if (!isInDialog) {
+      dialog.close();
+    }
+  });
